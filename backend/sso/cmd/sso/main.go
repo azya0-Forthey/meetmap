95669e782a485f99e471c1e6b08e2fac620c1455
Nikita <0forthey0@gmail.com>
2024-11-27 16:04:56 +0300

    Add logger

--- backend/sso/cmd/sso/main.go
+++ backend/sso/cmd/sso/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
 )
 
 func main() {
 	conf := config.MustLoad()
 
-	fmt.Println(conf)
+	log := setupLogger(conf.Env)
+
+	log.Info("starting application",
+		slog.String("env", conf.Env),
+		slog.Any("config", conf),
+		slog.Int("port", conf.GRPConfig.Port),
+	)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "prod":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	case "local":
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+
+	return log
 }
