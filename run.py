e0fc04d8a6e65b916edf76f7c302f98ae1672c99
Nikita <0forthey0@gmail.com>
2024-11-28 06:27:27 +0300

    Enhance structrure and add migrations into compose cluster

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import os
+import sys
 
+result = ""
+for arg in sys.argv[1:]:
+    result += arg + " "
 try:
-    os.system("docker-compose --env-file backend/db.yaml --env-file backend/server/config.yaml --env-file .env up --build")
+    os.system(f"docker-compose --env-file backend/db.yaml --env-file backend/server/config.yaml --env-file .env up --build {result}")
 except KeyboardInterrupt:
     exit(0)
