f557601a6008f7668846e53834e959fe123785bf
Nikita <0forthey0@gmail.com>
2024-11-30 16:29:20 +0300

    Add 'up' and 'down' command to run.py

--- run.py
+++ run.py
@@ -10,8 +10,20 @@ args_alias = {**args_alias,
     "detach": "-d"
 }
 
+
+if len(sys.argv) < 3:
+    print("Too few arguments")
+    exit(0)
+
+base_command = sys.argv[1]
+
+if base_command == "up":
+    base_command = "up --build"
+else:
+    base_command = "down"
+
 result = ""
-for arg in sys.argv[1:]:
+for arg in sys.argv[2:]:
     if arg == "all":
         result = f"{args[0]} {args[1]} {args[2]}"
         continue
@@ -22,6 +34,6 @@ for arg in sys.argv[1:]:
         exit(0)
     result += args_alias[arg] + " "
 try:
-    os.system(f"docker-compose --env-file backend/db.yaml --env-file backend/server/config.yaml --env-file .env up --build {result}")
+    os.system(f"docker-compose --env-file backend/db.yaml --env-file backend/server/config.yaml --env-file .env {base_command} {result}")
 except KeyboardInterrupt:
     exit(0)
